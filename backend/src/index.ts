41c95445481cae9fd83be7b2f91a034233bc49ce
Bunyamin <bunyamin2002@mail.ru>
2025-07-04 17:33:13 +0400

    feat: added rooms json data and rooms/search endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,49 @@
 import express, { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get("/api/data", (req: Request, res: Response) => {
-  const sampleData = {
-    message: "It is testing",
-    item: [1, 2, 3, 4, 5],
-  };
+const filePath = path.join(__dirname, "./Database/rooms.json");
 
-  res.json(sampleData);
+const fileContent = fs.readFileSync(filePath, "utf-8");
+const roomsData = JSON.parse(fileContent);
+
+// all rooms
+app.get("/api/rooms", (req: Request, res: Response) => {
+  try {
+    res.json(roomsData);
+  } catch (err) {
+    console.log("Error reading rooms data !");
+    res.status(500).json({ error: "### Failed to read rooms data" });
+  }
 });
 
-app.listen(PORT, () => console.log("Server running at your hand !"));
+// filter rooms
+
+app.post("/api/rooms/search", (req: Request, res: Response) => {
+  try {
+    const { checkIn, checkOut, adults, children, rooms } = req.body;
+
+    if (!checkIn || !checkOut || !adults || !children || !rooms) {
+      res.status(400).json({ error: "Doldurulmayan sahələr qalıb" });
+    }
+
+    const totalGuests = Number(adults) + Number(children);
+    const roomsNeeded = Number(rooms);
+
+    const filteredRooms = roomsData.rooms.filter((room: any) => {
+      const totalCapacity = room.capacity * roomsNeeded;
+      return room.available === true && totalCapacity >= totalGuests;
+    });
+    
+    res.json({ rooms: filteredRooms });
+  } catch (err) {
+    res.status(500).json({ error: "### Failed while filtering rooms data" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running at localhost://${PORT} !`));
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -6,15 +6,27 @@ import apiRequest from "../api/apiRequest";
 
 const PageLayout = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
-    const response = apiRequest({
-      method: "GET",
-      url: "api/data",
-      onError: (err) => console.log(err),
-      onSuccess: () => console.log("Completed"),
-    });
+    const fetchData = async () => {
+      const response = await apiRequest({
+        method: "POST",
+        url: "api/rooms/search",
+        data: {
+          checkIn: "2025-07-01",
+          checkOut: "2025-07-06",
+          adults: 2,
+          children: 1,
+          rooms: 2,
+        },
+        onError: (err) => console.log(err),
+        onSuccess: () => console.log("Data FETCHED !"),
+      });
 
-    console.log(response);
+      console.log(response);
+    };
+
+    fetchData();
   }, []);
+
   return (
     <>
       <GuestHeader />
