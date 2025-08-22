49c26f588441b0c91b53dc7640506923684587c9
Bunyamin <bunyamin2002@mail.ru>
2025-08-22 07:40:15 +0400

    added error message functionality for Input component

--- frontend/src/components/input/Input.tsx
+++ frontend/src/components/input/Input.tsx
@@ -11,6 +11,8 @@ interface TypeInput {
   style?: any;
   icons?: ReactNode | ReactNode[];
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string | null;
+  clearError?: () => void;
 }
 
 const Input = ({
@@ -20,23 +22,32 @@ const Input = ({
   placeholder,
   name,
   label,
-  errorMessage,
   style,
   onChange,
-  icons,
+  icons,  
+  error,
+  clearError,
 }: TypeInput) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e);
+    clearError?.();
+  };
   return (
     <div style={style} className="flex flex-col gap-1 items-star w-full">
       <label className="text-[14px] text-[#000] font-normal">{label}</label>
-      <div className="flex flex-row items-center justify-between  px-2 py-3 w-full bg-[#fff] rounded-[10px] border border-[#A6A6A6] text-[#A6A6A6]">
+      <div
+        className={`flex flex-row items-center justify-between  px-2 py-3 w-full bg-[#fff] rounded-[10px] border border-[#A6A6A6] text-[#A6A6A6] ${
+          error ? "border-2 border-[red]" : ""
+        }`}
+      >
         <input
-          className=" outline-none bg-none placeholder:text-[#A6A6A6]"
+          className={`outline-none bg-none placeholder:text-[#A6A6A6]`}
           type={type}
           name={name}
           value={value}
           defaultValue={defaultValue}
           placeholder={placeholder}
-          onChange={onChange}
+          onChange={handleChange}
         />
         {icons &&
           (Array.isArray(icons) ? (
@@ -49,7 +60,7 @@ const Input = ({
             <span className="cursor-pointer">{icons}</span>
           ))}
       </div>
-      <span>{errorMessage}</span>
+      <span className="text-[12px] text-[red]">{error && error}</span>
     </div>
   );
 };
--- frontend/src/pages/Auth/Login/index.tsx
+++ frontend/src/pages/Auth/Login/index.tsx
@@ -3,23 +3,57 @@ import CloseEye from "../../../assets/svg/closeEye.svg?react";
 import OpenEye from "../../../assets/svg/openEye.svg?react";
 import Button from "../../../components/button/Button";
 import apiRequest from "../../../api/apiRequest";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Login = () => {
   const [email, setMail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [isRemember, setIsRemember] = useState(false);
   const handleLogin = async () => {
+    const newErrors: { [key: string]: string } = {};
+
+    if (!email) {
+      newErrors["email"] = "Email daxil edilməyib";
+    }
+    if (!password) {
+      newErrors["password"] = "Şifrə daxil edilməyib";
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setError((prev) => ({ ...prev, ...newErrors }));
+      return;
+    }
+
     try {
       const res = await apiRequest({
         method: "POST",
         url: "/auth/login",
         data: { email, password },
       });
+
+      if (isRemember) {
+        localStorage.setItem("refreshToken", res.refreshToken);
+        localStorage.setItem("accessToken", res.accessToken);
+      } else {
+        sessionStorage.setItem("refreshToken", res.refreshToken);
+        sessionStorage.setItem("accessToken", res.accessToken);
+      }
     } catch (err) {
       console.log("LOGIN ERRROR : ", err);
     }
   };
+  const [error, setError] = useState<{ [key: string]: string }>({});
+
+  const getError = (field: string) => {
+    if (!field) return null;
+    console.log(error[field]);
+    return error[field];
+  };
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
   return (
     <>
       <div className="flex flex-col gap-4 w-full">
@@ -27,6 +61,7 @@ const Login = () => {
         <p className="text-[12px] font-semibold">Login to access account</p>
         <Input
           label="Email"
+          error={getError("email")}
           placeholder="Testing"
           value={email || ""}
           onChange={(e) => setMail(e.target.value)}
@@ -35,20 +70,28 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password || ""}
           type="password"
+          error={getError("password")}
           label="Password"
           placeholder="********"
           icons={<CloseEye className="w-[20px] h-[20px]" />}
         />
         <div className="flex flex-row items-center justify-between">
           <span className="flex flex-row items-center gap-2 justify-between text-[14px] text-[#121]">
-            <input type="checkbox" className="cursor-pointer" />
+            <input
+              type="checkbox"
+              onClick={() => setIsRemember(!isRemember)}
+              className="cursor-pointer"
+            />
             Remember Me
           </span>
           <p className="text-[#07689F] text-[14px]">Forgot password?</p>
         </div>
+        {getError("email") && <span>{getError("email")}</span>}
         <Button title="Login" onClick={handleLogin} />
       </div>
       <span className="text-[14px] text-[#000] font-normal">Or</span>
+      <button onClick={() => {}}>Add Error Message</button>
+      <button onClick={() => {}}>Get error</button>
     </>
   );
 };
