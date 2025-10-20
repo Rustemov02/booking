8edf80b43b77a0705922d74e1b70f9d3a3d0cea0
Bunyamin <bunyamin2002@mail.ru>
2025-10-20 21:34:47 +0400

    fixed backend deploy bug

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import apiRequest from "./apiRequest";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
